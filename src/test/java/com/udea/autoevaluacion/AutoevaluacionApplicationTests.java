ee5599dae5db6e4479b01b24f028a9f750327048
WilmerSoto <wilmer.soto@udea.edu.co>
2025-03-31 11:37:13 -0500

    Project start

---
+++ src/test/java/com/udea/autoevaluacion/AutoevaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.autoevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoevaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
