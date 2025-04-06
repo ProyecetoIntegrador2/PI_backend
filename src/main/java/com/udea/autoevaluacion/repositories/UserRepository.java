d1227c7468a49a68f70a03fdbb162f995c4a62f0
WilmerSoto <wilmer.soto@udea.edu.co>
2025-04-05 21:00:54 -0500

    implement CompanyController and CompanyService

--- src/main/java/com/udea/autoevaluacion/repositories/UserRepository.java
+++ src/main/java/com/udea/autoevaluacion/repositories/UserRepository.java
@@ -2,12 +2,12 @@ package com.udea.autoevaluacion.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.udea.autoevaluacion.models.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 } 
---
+++ src/main/java/com/udea/autoevaluacion/services/CompanyService.java
@@ -0,0 +1,24 @@
+package com.udea.autoevaluacion.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.udea.autoevaluacion.dtos.CompanyDTO;
+import com.udea.autoevaluacion.repositories.CompanyRepository;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<CompanyDTO> getAllCompanies() {
+        return companyRepository.findAll().stream()
+                .map(company -> new CompanyDTO(company.getId(), company.getName(), company.getOrganizationType(), company.getOrganizationSector(), company.getCountry(), company.getNumberOfEmployees()))
+                .collect(Collectors.toList());
+    }
+}
