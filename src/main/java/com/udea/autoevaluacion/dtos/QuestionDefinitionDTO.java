9735fadacb17042f327cd7ab67e34a022f8d3d3d
WilmerSoto <wilmer.soto@udea.edu.co>
2025-04-23 21:06:39 -0500

    add FormDefinitionDTO, PartDefinitionDTO, and QuestionDefinitionDTO; update SubmissionMetricsDTO with new fields and validation

---
+++ src/main/java/com/udea/autoevaluacion/dtos/QuestionDefinitionDTO.java
@@ -0,0 +1,5 @@
+package com.udea.autoevaluacion.dtos;
+
+public class QuestionDefinitionDTO {
+    
+}
--- src/main/java/com/udea/autoevaluacion/dtos/SubmissionMetricsDTO.java
+++ src/main/java/com/udea/autoevaluacion/dtos/SubmissionMetricsDTO.java
@@ -1,5 +1,33 @@
 package com.udea.autoevaluacion.dtos;
 
-public class SubmissionMetricsDTO {
+import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubmissionMetricsDTO {
+    @NotBlank(message = "El ID de la parte del formulario no puede ser vacío")
+    private String submissionPartId;
+
+    @NotBlank(message = "La metrica promedio del puntaje actual no puede ser vacía")
+    private int averageActualScore;
+
+    @NotBlank(message = "La metrica promedio del puntaje deseado no puede ser vacía")
+    private int averageDesiredScore;
+
+    @NotBlank(message = "El criterio de mayoría calificada no puede ser vacío")
+    private BigDecimal qualifiedMajorityCriterion;
+
+    @NotBlank(message = "El punto de corte mayoritario no puede ser vacío")
+    private Integer majorityCutOffLevel;
+
+    @NotBlank(message = "El puntaje basado en umbral no puede ser vacío")
+    private BigDecimal thresholdBasedScoring;
 }
