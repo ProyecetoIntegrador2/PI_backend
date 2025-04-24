bfaffa657224dc1fa8cfc202a22537637e79fef5
WilmerSoto <wilmer.soto@udea.edu.co>
2025-04-24 00:36:09 -0500

    add transaction management to CompanyService, LoginService, and RegisterService; create SubmissionService with initial structure

--- src/main/java/com/udea/autoevaluacion/services/CompanyService.java
+++ src/main/java/com/udea/autoevaluacion/services/CompanyService.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.udea.autoevaluacion.dtos.CompanyDTO;
 import com.udea.autoevaluacion.repositories.CompanyRepository;
+
 
 @Service
 public class CompanyService {
@@ -16,6 +18,7 @@ public class CompanyService {
         this.companyRepository = companyRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<CompanyDTO> getAllCompanies() {
         return companyRepository.findAll().stream()
                 .map(company -> new CompanyDTO(company.getId(), company.getName(), company.getOrganizationType(), company.getOrganizationSector(), company.getCountry(), company.getNumberOfEmployees()))
