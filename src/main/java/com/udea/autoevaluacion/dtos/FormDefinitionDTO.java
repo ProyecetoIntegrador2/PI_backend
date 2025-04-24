c37165c9c8b2fe59ae77a1b8c9273228a34ea115
WilmerSoto <wilmer.soto@udea.edu.co>
2025-04-23 21:15:48 -0500

    update FormDefinitionDTO, PartDefinitionDTO, and QuestionDefinitionDTO

--- src/main/java/com/udea/autoevaluacion/dtos/FormDefinitionDTO.java
+++ src/main/java/com/udea/autoevaluacion/dtos/FormDefinitionDTO.java
@@ -1,5 +1,18 @@
 package com.udea.autoevaluacion.dtos;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FormDefinitionDTO {
-    
+    private String name;
+    private String version;
+    private List<PartDefinitionDTO> partDefinitions;
 }
--- src/main/java/com/udea/autoevaluacion/dtos/QuestionDefinitionDTO.java
+++ src/main/java/com/udea/autoevaluacion/dtos/QuestionDefinitionDTO.java
@@ -1,5 +1,15 @@
 package com.udea.autoevaluacion.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuestionDefinitionDTO {
-    
+    private int questionNumber;
+    private String questionText;
 }
